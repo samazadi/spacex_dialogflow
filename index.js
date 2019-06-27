4e89010586b701b12d298c4165fc7a87f90a42c0
Sam <samg.azadi@gmail.com>
2019-06-26 21:02:03 -0700

    minor refactor

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const axios = require('axios');
 
 const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
-// const { Card, Suggestion } = require('dialogflow-fulfillment');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const baseUrl = 'https://api.spacexdata.com/v3';
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
@@ -25,7 +25,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
   function getInfoByYearHandler(agent) {
     const launchYear = agent.parameters.launchYear;
-    const url = 'https://api.spacexdata.com/v3/launches';
+    const url = `${baseUrl}/launches`;
     return axios.get(`${url}?launch_year=${launchYear}&order=desc`)
       .then(res => {
         const launchData = res.data;
@@ -34,7 +34,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   }
 
   function getMissionDataHandler(agent) {
-    const url = 'https://api.spacexdata.com/v3/missions';
+    const url = `${baseUrl}/missions`;
     agent.add('Some recent missions are:');
     return axios.get(`${url}?limit=3`)
       .then(res => {
@@ -46,7 +46,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   }
 
   function getInfoDataHandler(agent) {
-    const url = 'https://api.spacexdata.com/v3/info';
+    const url = `${baseUrl}/info`;
     return axios.get(url)
       .then(res => {
         const info = res.data;
